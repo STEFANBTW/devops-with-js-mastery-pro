4b5928b5dc26bdcc34e7770afedd92d72d9f7793
Your Name <you@example.com>
2026-03-19 19:19:51 +0100

    Implement eslint and prettier

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from DevOps Mastery Pro!');
+  res.status(200).send('Hello from DevOps Mastery Pro!');
 });
 
 export default app;
